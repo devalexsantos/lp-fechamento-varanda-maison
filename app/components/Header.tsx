6322cf7cb421ff5c7c732089eb30011ce50462b0
devalexsantos <devalexsantos@gmail.com>
2024-10-02 14:53:45 +0000

    chore: create a negative points section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,11 +2,9 @@ import Image from 'next/image'
 
 export function Header() {
   return (
-    <div className="min-h-[50vh] w-full flex flex-col bg-[url('/bg-hero.png')] bg-cover items-center relative">
-      <div className="mt-4 flex w-full items-center justify-between gap-3 bg-[#185794] bg-opacity-90 p-6">
-        <h1 className="text-2xl leading-8 text-white font-bold w-full max-w-[300px]">Quer o melhor <strong className="text-[#FE9821]">fechamento de varanda</strong> do Brasil?</h1>
-        <Image src="/maison-logo-white.png" alt="Maison do Vidro" width={220} height={77} quality={100} />
-      </div>
+    <div className="min-h-[50vh] w-full flex flex-col bg-[url('/bg-hero.png')] bg-cover items-center relative"> <div className="mt-4 flex w-full items-center justify-between gap-3 bg-[#185794] bg-opacity-90 p-6">
+      <h1 className="text-2xl leading-8 text-white font-bold w-full max-w-[300px]">Quer o melhor <strong className="text-[#FE9821]">fechamento de varanda</strong> do Brasil?</h1> <Image src="/maison-logo-white.png" alt="Maison do Vidro" width={220} height={77} quality={100} />
+    </div>
       <div className="bg-white rounded-2xl flex flex-col justify-center items-center  w-full max-w-[350px] absolute z-10 bottom-[-20px] text-center font-black px-2 py-4">
         <Image src="/pin.svg" alt="Pin" width={15} height={15} className="mt-[-30px]" />
         <span className="text-4xl text-primary">Reiki original</span>
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,36 @@ export default function Home() {
         <WhatsAppButton />
         <Image src="/arrow-down.svg" alt="Arrow Down" width={50} height={50} className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-[-30px]" />
       </div>
+
+      <div className="flex flex-col gap-12 items-center px-2 pt-8 pb-12 bg-[url('/bg-features.png')] bg-cover border-b-8 border-b-[#063A6C]">
+        <div className="w-full max-w-[350px] text-white text-center bg-primary opacity-80 mt-6 p-4 rounded-2xl border-4 border-primary shadow-lg">
+          <span className="text-3xl text-center text-white font-black">Aproveita pouco sua varanda? </span>
+        </div>
+
+        <div className="w-full flex flex-col gap-3 max-w-[350px] text-white bg-primary opacity-80 mt-6 p-4 rounded-2xl border-4 border-primary shadow-lg">
+          <div className="flex items-center gap-3">
+            <Image src="/x-plus-icon.svg" width={44} height={45} alt="X" />
+            <span className="text-2xl text-white font-bold">
+              Chuva e Vento
+            </span>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Image src="/x-plus-icon.svg" width={44} height={45} alt="X" />
+            <span className="text-2xl text-white font-bold">
+              Ambiente desagradável
+            </span>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Image src="/x-plus-icon.svg" width={44} height={45} alt="X" />
+            <span className="text-2xl text-white font-bold">
+              Falta de integração com a sala
+            </span>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
