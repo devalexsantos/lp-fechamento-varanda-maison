86d208c48f180b75529fe55a5a8291a0ebf1d4d1
devalexsantos <devalexsantos@gmail.com>
2024-10-08 18:01:14 +0000

    chore: add videos link url

--- app/components/sections/ProTips.tsx
+++ app/components/sections/ProTips.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function ProTips() {
   return (
@@ -8,8 +9,14 @@ export function ProTips() {
         <h4 className="text-xl font-bold w-full text-center">Dicas de arquitetos</h4>
         <p className="text-xl text-center">Veja alguns exemplos de integração de ambientes com Reiki:</p>
         <div className="flex flex-col gap-4 md:flex-row">
-          <Image src="/dicas-de-arquitetos-1.png" alt="Dicas de arquitetos" width={350} height={145} />
-          <Image src="/dicas-de-arquitetos-2.png" alt="Dicas de arquitetos" width={350} height={145} />
+
+          <Link href="https://youtu.be/YXrdYXAgNfc" target="_blank">
+            <Image src="/dicas-de-arquitetos-1.png" alt="Dicas de arquitetos" width={350} height={145} />
+          </Link>
+
+          <Link href="https://youtu.be/vg60BLf4Mzc" target="_blank">
+            <Image src="/dicas-de-arquitetos-2.png" alt="Dicas de arquitetos" width={350} height={145} />
+          </Link>
         </div>
       </div>
     </div>
