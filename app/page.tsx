be36d4c084f7f62aedba827b61522636909f6fbc
devalexsantos <devalexsantos@gmail.com>
2024-10-08 18:09:15 +0000

    chore: fix arrow down position

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
-      <div className="flex flex-col items-center gap-3 bg-primary text-center px-6 pt-10 pb-12">
+      <div className="flex flex-col items-center gap-3 bg-primary text-center px-6 pt-10 pb-8">
         <div className="mt-[-70px] z-10 bg-white rounded-2xl flex flex-col justify-center items-center  w-full max-w-[300px] text-center font-black px-2 py-4">
           <Image src="/pin.svg" alt="Pin" width={15} height={15} className="mt-[-30px]" />
           <span className="text-4xl text-primary">Reiki original</span>
@@ -23,7 +23,7 @@ export default function Home() {
 
         <span className="text-3xl text-white text-center w-full max-w-[350px]">Somos a única empresa autorizada na Bahia</span>
         <WhatsAppButton color="green" />
-        <Image src="/arrow-down.svg" alt="Arrow Down" width={50} height={50} className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-[-30px]" />
+        <Image src="/arrow-down.svg" alt="Arrow Down" width={50} height={50} className="mt-4" />
       </div>
       <NegativePoints />
       <PositivePoints />
