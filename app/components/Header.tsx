3dd883cf4f67dabc4eff2824bba4cb40ee2bfea6
devalexsantos <devalexsantos@gmail.com>
2024-10-08 17:57:40 +0000

    chore: move reiki box call to page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,11 +7,5 @@ export function Header() {
         <h1 className="text-2xl md:text-4xl leading-8 text-white font-bold w-full max-w-[300px] md:max-w-[800px]">Quer o melhor <strong className="text-[#FE9821]">fechamento de varanda</strong> do Brasil?</h1>
         <Image src="/maison-logo-white.png" alt="Maison do Vidro" width={220} height={77} quality={100} />
       </div>
-      <div className="bg-white rounded-2xl flex flex-col justify-center items-center  w-full max-w-[350px] absolute z-10 bottom-[-20px] text-center font-black px-2 py-4">
-        <Image src="/pin.svg" alt="Pin" width={15} height={15} className="mt-[-30px]" />
-        <span className="text-4xl text-primary">Reiki original</span>
-        <Image src="/separator.svg" alt="Separator" width={0} height={0} className="w-full max-w-[300px]" />
-        <span className="text-4xl text-orange">SÓ AQUI</span>
-      </div>
     </div>)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,14 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <Header />
-      <div className="flex flex-col items-center gap-3 bg-primary text-center px-6 pt-10 pb-12 relative">
+      <div className="flex flex-col items-center gap-3 bg-primary text-center px-6 pt-10 pb-12">
+        <div className="mt-[-70px] z-10 bg-white rounded-2xl flex flex-col justify-center items-center  w-full max-w-[300px] text-center font-black px-2 py-4">
+          <Image src="/pin.svg" alt="Pin" width={15} height={15} className="mt-[-30px]" />
+          <span className="text-4xl text-primary">Reiki original</span>
+          <Image src="/separator.svg" alt="Separator" width={0} height={0} className="w-full max-w-[300px]" />
+          <span className="text-4xl text-orange">SÓ AQUI</span>
+        </div>
+
         <span className="text-3xl text-white text-center w-full max-w-[350px]">Somos a única empresa autorizada na Bahia</span>
         <WhatsAppButton color="green" />
         <Image src="/arrow-down.svg" alt="Arrow Down" width={50} height={50} className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-[-30px]" />
