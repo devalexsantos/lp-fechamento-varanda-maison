a43f96a2c79f98e220b7e5de88b4359ff68ea401
devalexsantos <devalexsantos@gmail.com>
2024-10-11 12:29:45 +0000

    chore: adjusts on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,24 +7,46 @@ export function Footer() {
 
       <div className="w-full max-w-[764px] flex flex-col gap-8 items-center">
         <p className="text-left md:text-center text-xl">
-          Há <strong className="font-black">14 anos</strong> trabalhamos com as melhores marcas e sistemas de envidraçamento do mercado.
+          Há <strong className="font-black">14 anos</strong> trabalhamos com as melhores marcas{' '}
+          e sistemas de envidraçamento do mercado.
+        </p>
+
+
+        <p className="text-left md:text-center text-xl">
           Oferecemos soluções completas em vidros para apartamentos, casas e empresas.
         </p>
 
-        <p className="text-left md:text-center text-xl">
-          Sistema Reiki - Esquadrias Roll Door - Box - Espelhos - Guarda Corpo, Vidros Blindex e Habitat e muito mais.
-        </p>
+        <div className="flex gap-4">
+          <Image src="/logos-partners-reiki/logo-reiki-blue.png" alt="Reiki" width={200} height={200} quality={100} />
+          <Image src="/logos-partners-reiki/logo-blindex.png" alt="Blindex" width={200} height={200} quality={100} />
+          <Image src="/logos-partners-reiki/logo-roll-door.png" alt="Roll Door" width={200} height={200} quality={100} />
+        </div>
 
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="flex gap-4">
-            <Image src="/logos-partners-reiki/reiki.jpg" alt="Reiki" width={120} height={120} />
-            <Image src="/logos-partners-reiki/blindex.jpg" alt="Blindex" width={120} height={120} />
-          </div>
+        <div className="flex flex-col gap-2">
+          <span className="flex gap-2 items-center text-xl">
+            <Image src="/check-green.svg" alt="Check" width={24} height={24} quality={100} />
+            Fechamento de varanda Reiki
+          </span>
 
-          <div className="flex gap-4">
-            <Image src="/logos-partners-reiki/habitat.jpg" alt="Habitat" width={120} height={120} />
-            <Image src="/logos-partners-reiki/roll-dor-logo.jpg" alt="Roll Door" width={120} height={120} />
-          </div>
+          <span className="flex gap-2 items-center text-xl">
+            <Image src="/check-green.svg" alt="Check" width={24} height={24} quality={100} />
+            Esquadrias de alto padrão Roll Door
+          </span>
+
+          <span className="flex gap-2 items-center text-xl">
+            <Image src="/check-green.svg" alt="Check" width={24} height={24} quality={100} />
+            Box e Espelhos especiais
+          </span>
+
+          <span className="flex gap-2 items-center text-xl">
+            <Image src="/check-green.svg" alt="Check" width={24} height={24} quality={100} />
+            Guarda Corpo, Telhado de vidro e muito mais
+          </span>
+
+          <span className="flex gap-2 items-center text-xl">
+            <Image src="/check-green.svg" alt="Check" width={24} height={24} quality={100} />
+            Vidros  Blindex e Habiat
+          </span>
 
         </div>
 
