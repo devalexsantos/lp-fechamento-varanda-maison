201db0b5785380de0b5dd548cab745ef7260d2ae
devalexsantos <devalexsantos@gmail.com>
2024-10-14 11:53:50 +0000

    chore: fix responsive layout

--- app/components/sections/ProTips.tsx
+++ app/components/sections/ProTips.tsx
@@ -1,11 +1,11 @@
 export function ProTips() {
   return (
-    <div className="flex flex-col gap-4 px-4 py-12 text-primary">
-      <h3 className="text-3xl font-black text-center">Os melhores profissionais indicam <strong className="text-orange">Reiki</strong></h3>
-      <div className="flex flex-col gap-4 items-center">
+    <div className="w-full flex flex-col gap-4 px-4 py-12 text-primary">
+      <h3 className="w-full text-3xl font-black text-center">Os melhores profissionais indicam <strong className="text-orange">Reiki</strong></h3>
+      <div className="w-full flex flex-col gap-4 items-center">
         <h4 className="text-xl font-bold w-full text-center">Dicas de arquitetos</h4>
         <p className="text-xl text-center">Veja alguns exemplos de integração de ambientes com Reiki:</p>
-        <div className="flex flex-col gap-4 md:flex-row">
+        <div className="w-full flex flex-col gap-4 items-center">
 
           <iframe
             className="w-full max-w-[764px] h-[315px] md:h-[560px]"
