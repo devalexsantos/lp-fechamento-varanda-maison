98fcfaac62faa25d9de8411e741833804f2f427e
devalexsantos <devalexsantos@gmail.com>
2024-10-18 11:32:37 +0000

    chore: change whatsapp redirect link

--- app/components/ui/WhatsAppButton.tsx
+++ app/components/ui/WhatsAppButton.tsx
@@ -7,7 +7,7 @@ type WhatsAppButtonProps = {
 
 export function WhatsAppButton({ color }: WhatsAppButtonProps) {
   return (
-    <Link data-color={color} href="https://web.whatsapp.com/send?phone=5571999993553" target="_blank" className="flex w-full max-w-[350px] items-center gap-2 justify-center rounded-xl 
+    <Link data-color={color} href="https://wa.me/5571999993553" target="_blank" className="flex w-full max-w-[350px] items-center gap-2 justify-center rounded-xl 
       p-4 data-[color=green]:bg-[#22A800] 
       data-[color=orange]:bg-[#FF8B02]
       text-center text-white font-black text-xl">
